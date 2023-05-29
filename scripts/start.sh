bda10f6a37d2fc3da7bef4f00a274b8dbd4a9f3c
Sergey Yakovlev <fclaw007@gmail.com>
2023-05-29 11:53:44 +0300

    script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
-sha=$(command git log -n 1 --pretty=format:"%H")
+rm .env
+
+sha=$(git rev-parse HEAD@{$1})
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 tag="${branch}_${sha}"
 
-echo "TAG=$tag" > .env
+cat <<EOT >> .env
+  DBUSER=sonny
+  DATABASE=scaffold
+  TAG=$tag
+EOT
 
-if [ $# -ge 1 ] && [ -n "$1" ]
+if [ $# -ge 1 ] && [ -n "$2" ]
 then 
   composer=$1
 
-  if [ $composer == "yes" ] ; then
-   exec docker-compose up
+  if [ $composer == "yes" ]
+  then
+     exec docker-compose up
+  else
+     echo "arg must be yes" 
   fi
 fi
