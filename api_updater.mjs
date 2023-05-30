ad3dd47a0f44b99fe09d7e4c9e5abd16ccdaacee
Sergey Yakovlev <fclaw007@gmail.com>
2023-05-30 13:57:58 +0300

    fix

--- api_updater.mjs
+++ api_updater.mjs
@@ -2,7 +2,7 @@ import * as https from 'https'; // or 'https' for https:// URLs
 import * as fs from 'fs';
 
 const file = fs.createWriteStream("api.json");
-const request = https.get("https://8796-78-180-9-117.ngrok-free.app/swagger.json", function(response) {
+const request = https.get("https://4ec4-78-180-9-117.ngrok-free.app/swagger.json", function(response) {
    response.pipe(file);
 
    // after download completed close filestream
